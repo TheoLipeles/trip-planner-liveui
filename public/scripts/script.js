fd30e4a380da9be79a36fdf253dd96dee6a3651a
bryan chu <shino.sk@gmail.com>
2015-07-06 02:24:09 -0400

    refactor script.js, index.html and completed Remove from Itinerary

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,108 +1,79 @@
+//this is a self invoking function, equals document.ready
+(function(){
 
-// var $addHotelBtn = $(".hotel-selector .btn");
-// var $myHotel = $(".panel-body .my_hotel .list-group");
-
-var itinerary_div = '<div class="itinerary-item" id="@ID@"></div>';
-var itinerary_span = '<span class="title">@NAME@</span><button class="btn btn-xs btn-danger remove btn-circle">x</button>';
-
-
-// $addHotelBtn.on("click", function() {
-// //	$addHotelBtn.css("background", "#000");
-// 	// $(this).css("background", "#000");
-
-
-// 	var hotel_id = $(".hotel-selector select").val();
-
-// 	$.each(all_hotels, function(i) {
-// 		var hotel = all_hotels[i];
-// 		if (hotel._id === hotel_id) {
-// 			if ($myHotel.children().length > 0) {
-				//$($($myHotel.children()[0]).attr("id", hotel_id).children()[0]).html(hotel.name);
-// 			} else {
-// 				$myHotel.append(itinerary_div.replace("@HOTEL_ID@", hotel_id));
-// 				$("#@HOTEL_ID@.itinerary-item".replace("@HOTEL_ID@", hotel_id)).append(itinerary_span.replace("@HOTEL_NAME@", hotel.name));
-// 			}
-// 		}
-// 	});
-
-// });
-
-function getObjectById(obj,id) {
-	for (var i = 0; i < obj.length; i++) {
-		if(obj[i]._id===id) {
-			return obj[i];
-		}
-	}
+// this is to maintain the data and prevent naming confusion;
+var events = {
+	hotel: hotel,
+	restaurant: restaurant,
+	activity: activity
 }
 
-function addItem(target, id, name, obj, image){
-		target.append(itinerary_div.replace("@ID@", id));
-		$("#@ID@.itinerary-item".replace("@ID@", id)).append(itinerary_span.replace("@NAME@", name));
-		drawLocation(obj.place[0].location, {icon: "/images/" + image});
-}
+//this is the main event handler for all the events
+function addEventItem(event_type, image, occurrence){
 
-
-$(".btn.btn-primary.btn-circle.pull-right").on("click", function() {
-	var parent = $(this).parent();
-	var id = $($(this).siblings("select")[0]).val();
-	var target;
-	var name;
-	var new_s;
-	var obj;
-	if(parent.hasClass('hotel-selector')){
-		target = $('.panel-body.itinerary .my_hotel .list-group');
-		obj = getObjectById(all_hotels, id);
-		name = obj.name;
-		if($(target.children()).length > 0) {
-			$($(target.children()[0]).attr("id", id).children()[0]).html(name);
-		} else {
-			addItem(target, id, name, obj, "lodging_0star.png");
-		}
-	} else if(parent.hasClass('restaurant-selector')){
-		target = $('.panel-body.itinerary .my_restaurants .list-group');
-		new_s = $('.panel-body.itinerary .my_restaurants .list-group #' + id);
-		if($(target.children()).length <3 && new_s.length ===0) {
-			obj = getObjectById(all_restaurants, id);
-			name =obj.name;
-			addItem(target, id, name, obj, "restaurant.png");
-		}
-	} else if(parent.hasClass('thing-selector')){
-		target = $('.panel-body.itinerary .my_things .list-group');
-		new_s = $('.panel-body.itinerary .my_things .list-group #' + id);
-		if(new_s.length ===0) {
-			obj = getObjectById(all_things_to_do, id);
-			name = obj.name;
-			addItem(target, id, name, obj, "star-3.png");
+	// helper function to find the exact event_type
+	function getEventById(obj, id) {
+		for (var i = 0; i < obj.length; i++) {
+			if(obj[i]._id===id) {
+				return obj[i];
+			}
 		}
 	}
-	$(".btn.remove").on("click", function() {
-		var parent = $(this).parent();
-		parent.remove();
-		console.log(locations, locations[0]);
+
+	// dom elements to manipulate
+	var event_handler = $('.' + event_type + '-selector' + '>.btn.btn-primary.btn-circle.pull-right');
+	event_handler.on('click', function(){
+		var parent = $(event_handler).parent();
+		var id = $($(event_handler).siblings("select")[0]).val();
+		var target_div = $('.panel-body.itinerary .my_' + event_type + ' .list-group');	
+		var obj = getEventById(events[event_type], id);
+
+		if(event_type !== 'hotel') {
+			var duplicate_div = $('.panel-body.itinerary .my_' + event_type + ' .list-group #' + id);
+		}		
+		// event_type conditions
+		switch(event_type) {
+			case 'hotel': 
+				if($(target_div.children()).length > occurrence) {
+					$(target_div.children()[0]).remove();
+					locations[0].setMap(null);
+					locations = locations.slice(1);
+				}
+				addItineraryItem(target_div, obj, image);
+				break;
+			case 'restaurant': 
+				if($(target_div.children()).length < occurrence && duplicate_div.length ===0) {
+					addItineraryItem(target_div, obj, image);
+				}
+				break;
+			case 'activity':
+				if(duplicate_div.length ===0) {
+					addItineraryItem(target_div, obj, image);
+				}
+				break;
+		}
+	});
+};
+
+//this is the event handler for all the itinerary items
+function addItineraryItem(target, event, image){
+	var itinerary_div = '<div class="itinerary-item" id="@ID@"></div>';
+	var itinerary_span = '<span class="title">@NAME@</span><button class="btn btn-xs btn-danger remove btn-circle">x</button>';
+
+	target.append(itinerary_div.replace("@ID@", event._id));
+	$("#@ID@.itinerary-item".replace("@ID@", event._id)).append(itinerary_span.replace("@NAME@", event.name)).on('click', function(){
+		$(this).remove();
 		locations[0].setMap(null);
 		locations = locations.slice(1);
 	});
-});
 
+	drawLocation(event.place[0].location, {icon: "/images/" + image});
+};
 
+//registering the 3 events
+addEventItem('hotel','lodging_0star.png', 0);
+addEventItem('restaurant','restaurant.png', 3);
+addEventItem('activity','star-3.png');
 
-	    // drawLocation(hotelLocation, {
-		    //     icon: '/images/lodging_0star.png'
-		    // });
-		    // restaurantLocations.forEach(function (loc) {
-		    //     drawLocation(loc, {
-		    //         icon: '/images/restaurant.png'
-		    //     });
-		    // });
-		    // thingToDoLocations.forEach(function (loc) {
-		    //     drawLocation(loc, {
-		    //         icon: '/images/star-3.png'
-		    //     });
-		    // });
-
-
-
-
-
-
+}());
 
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,24 @@
 var router = require('express').Router();
 
 var models = require('../models');
+var bluebird = require('bluebird');
 
-router.get('/',
-	function (req, res, next) {
-		models.Hotel
-			.find({})
-			.exec(function (err, hotels) {
-				// attach data to res.locals and then go on
-				res.locals.all_hotels = hotels;
-				next();
-			});
-	},
-	function (req, res, next) {
-		models.ThingToDo
-			.find({})
-			.exec(function (err, thingsToDo) {
-				// attach data to res.locals and then go on
-				res.locals.all_things_to_do = thingsToDo;
-				next();
-			});
-	},
-	function (req, res, next) {
-		models.Restaurant
-			.find({})
-			.exec(function (err, restaurants) {
-				// attach data to res.locals and then go on
-				res.locals.all_restaurants = restaurants;
-				next();
-			});
-	},
-	function (req, res) {
-		// all the data attached to res.locals will now be passed to the index template
-		res.render('index');
+router.get('/', function(req, res, next){
+	var hotelPromise = models.Hotel.find({}).exec();
+	var restaurantPromise = models.Restaurant.find({}).exec()
+	var thingsToDoPromise = models.ThingToDo.find({}).exec();
+	bluebird.join(hotelPromise, restaurantPromise, thingsToDoPromise)
+	.spread(function(a,b,c){
+		res.status(200).render('index', {
+			event_type:{
+				hotel: a,
+				restaurant: b,
+				activity: c
+			}
 	});
+	},function(err){
+		next();
+	});
+})
 
 module.exports = router;
